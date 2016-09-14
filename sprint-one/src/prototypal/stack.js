ce8cb09840ca8b972afc595323c74515daa2d6c1
vicmgs <vic.mgs@gmail.com>
2016-09-14 16:15:41 -0700

    Pass all tests for stack of prototypal pattern

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,32 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.counter = 0;
+  newStack.storage = {};
+
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(val) {
+  this.storage[this.counter] = val;
+  this.counter++;
+};
 
+stackMethods.pop = function() {
+  if (this.counter > 0) {
+    this.counter--;
+  }
+  var temp = this.storage[this.counter];
+  delete this.storage[this.counter];
+
+  return temp;
+  
+};
+
+stackMethods.size = function() {
+  return this.counter;
+};
