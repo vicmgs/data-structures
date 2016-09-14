6934bdcd3008107b8de3a07a666e4edeefcb6011
vicmgs <vic.mgs@gmail.com>
2016-09-14 15:33:12 -0700

    Pass all queue tests for functional pattern

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,18 +1,33 @@
 var Queue = function() {
   var someInstance = {};
 
+  var counter = 0;
+  var frontStack = 0;
+  var backStack = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[backStack] = value;
+    counter++;
+    backStack++;
   };
 
   someInstance.dequeue = function() {
+    if (counter > 0) {
+      counter--;
+    }
+    var temp = storage[frontStack];
+    delete(storage[frontStack]);
+    frontStack++;
+    return temp;
+
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
