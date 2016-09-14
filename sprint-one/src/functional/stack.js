f0f8984f219143002b92af7550522f71ba4c15b8
vicmgs <vic.mgs@gmail.com>
2016-09-14 15:03:12 -0700

    Pass tests for stack within the functional pattern

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,31 @@
 var Stack = function() {
   var someInstance = {};
 
+  var counter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[counter] = value;
+    counter++;
   };
 
   someInstance.pop = function() {
+    if (counter > 0) {
+      counter--;
+    }
+
+    var temp = storage[counter];
+    console.log('temp: ' + temp);
+    delete storage[counter];
+    console.log(storage);
+
+    return temp;
   };
 
   someInstance.size = function() {
+    return counter;
   };
 
   return someInstance;
