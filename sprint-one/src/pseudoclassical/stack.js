42be049d75ba45af8af641dd100dd23461496034
vicmgs <vic.mgs@gmail.com>
2016-09-14 16:31:53 -0700

    Pass all tests for stack in psuedo classical

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.counter = 0;
+
+};
+
+Stack.prototype.push = function(val) {
+  this.storage[this.counter] = val;
+  this.counter++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.counter > 0) {
+    this.counter--;
+  }
+  var temp = this.storage[this.counter];
+  delete this.storage[this.counter];
+
+  return temp;
+};
+
+Stack.prototype.size = function() {
+  return this.counter;
 };
 
 
