f8df897ff02954ac4dbf3c4d2403de78dad83260
vicmgs <vic.mgs@gmail.com>
2016-09-15 16:40:32 -0700

    Pass 3/4 tests for Binary Search Tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,45 @@
 var BinarySearchTree = function(value) {
+  this.value = value;
+  this.left = null;
+  this.right = null;
 };
 
 
+BinarySearchTree.prototype.insert = function(value) {
+  var nodeCreate = function() {
+    return new BinarySearchTree(value);
+  };
+
+  if (value > this.value) {
+    if (this.right !== null) {
+      this.right.insert(value);
+    } else {
+      this.right = nodeCreate(value);
+    }
+  } else {
+    if (this.left !== null) {
+      this.left.insert(value);
+    } else {        
+      this.left = nodeCreate(value); 
+    }
+  }
+};
+
+
+BinarySearchTree.prototype.contains = function(value) {
+  if (value === this.value) {
+    return true;
+  } else if (value > this.value) {
+    return this.right === null ? false : this.right.contains(value);
+  } else {
+    return this.left === null ? false : this.left.contains(value);
+  }
+};
+
+
+BinarySearchTree.prototype.depthFirstLog = function(cb) {
+  
+};
 /*
  * Complexity: What is the time complexity of the above functions?
  */
