912461dd22343e43071bff885ea3c5e6a6671191
vicmgs <vic.mgs@gmail.com>
2016-09-14 20:46:29 -0700

    Pass all tests for linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = new Node(value);
+    
+    if (list.head === null) {
+      list.head = newNode;  
+    } else {
+      list.tail.next = newNode;
+    }
+    list.tail = newNode;
   };
 
   list.removeHead = function() {
+    var temp = list.head.value;
+    list.head = list.head.next;
+
+    return temp;
   };
 
   list.contains = function(target) {
+    var arr = [];
+    var head = list.head;
+    
+    while (head.next !== null) {
+      arr.push(head.value);
+      head = head.next;
+    }
+    arr.push(list.tail.value);
+
+    if (arr.indexOf(target) !== -1) {
+      return true;
+    } else {
+      return false;
+    }
   };
 
   return list;
